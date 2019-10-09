343162ecbc4ed9ce707d134e6ca84286628a7c3b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-09 17:56:00 +0200

    added solution for rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,24 @@
 
 import sys
 
+def add_combinations(arr, index, result):
+  if index == len(arr):
+    result.append(list(arr))
+    return  
+  else:
+    for item in ("rock", "paper", "scissors"):
+      arr[index] = item
+      add_combinations(arr, index + 1, result)
+
 def rock_paper_scissors(n):
-  pass 
+  if n == 0:
+    return [[]]
+
+  result = []
+  num_player = [0] * n
+  add_combinations(num_player, 0, result)
+  return result
+
 
 
 if __name__ == "__main__":
